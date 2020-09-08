4f7b61a9c2edce33da398606d60bafd2942ffb55
sk440 <sk440@IN-5CG0251NDX>
2020-09-08 07:50:52 +0530

    commit

---
+++ JenkinsAssig/src/SimpleText.java
@@ -0,0 +1,8 @@
+
+public class SimpleText {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is Saritha, checking whether my github is working fine with Jenkins.");
+	}
+
+}
